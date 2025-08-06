123a7a40b7fd3822d3cc55b6083106d880a9cc5c
Mancaceresuade <mancaceres@uade.edu.ar>
2025-08-06 11:20:48 -0300

    ejemplo calculo complejidad cuadratica

--- clase1/clase1/src/App.java
+++ clase1/clase1/src/App.java
@@ -7,6 +7,30 @@ import java.util.Map;
 
 public class App {
 
+    public static double calcularPromedio(int[][] mat) {
+        // if(mat.length!=mat[0].length) throw new RuntimeException("calculo valido solo para matriz cuadrada");
+        int suma = 0; // 1
+        int cantidadFilasColumnas = mat.length; // 2  // porque es matriz cuadrada fila==columnas
+        for(int fila=0; fila < cantidadFilasColumnas; fila++){ // 1 + n + 1 + n
+            for(int col=0; col < cantidadFilasColumnas; col++){ // n ( 1 + n + 1 + n)
+                suma = suma + mat[fila][col]; // n ( 3 n )
+            }
+        }
+        return (suma/(cantidadFilasColumnas*cantidadFilasColumnas)); // 3
+    } // f(n) = 1 + 2 + 1 + n + 1 + n + n ( 1 + n + 1 + n) + n ( 3 n ) + 3  // aplico propiedad distributiva
+      // f(n) = 1 + 2 + 1 + n + 1 + n + n + n^2 + n + n^2 + 3n^2 + 3  // sumo
+      // f(n) = 8 + 4n + 5n^2
+      // f(n) <= c g(n)
+      // 8 + 4n + 5n^2 <= 6n^2  // tomo el termino dominante, sumo constante mas 1  
+      // 8/n^2 + 4n/n^2 + 5n^2/n^2 <= 6n^2/n^2  // simplifico
+      // 8/n^2 + 4n/n^2 + 5 <= 6  // siguiente, busco los valores n0 que cumplen
+      // para n = 1 cumple ?     no cumple (17<=6)
+      // para n = 2 cumple ?     no cumple (9<=6)
+      // para n = 3 cumple ?     no cumple (aprox 7 <= 6)
+      // para n = 4 cumple ?     no cumple
+      // para n = 5 cumple ?     cumple
+      // f(n) pertence a O(n^2)  para c = 6 y n0 >= 5,46
+
     public static void inprimirImpares(int[] numeros) {
         int aux = numeros.length; // 2
         for(int i=0; i < aux; i++) {  // 1 + (n+1) + n
@@ -36,7 +60,10 @@ public class App {
     public static void main(String[] args) {
 
         int[] numeros = {1, 2, 3, 4, 5};
-        System.out.println(inprimirImpares(numeros));
+        inprimirImpares(numeros);
+
+        int[][] mat = {{4,5,6},{7,8,9},{5,6,7}};
+        System.out.println(calcularPromedio(mat));
 
         /*
         List<Jugador> jugadores = new ArrayList<>();
