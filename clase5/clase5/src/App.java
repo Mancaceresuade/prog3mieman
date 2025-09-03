99107d4c7c1898d09a54307364d259ca44ee5f21
Mancaceresuade <mancaceres@uade.edu.ar>
2025-09-03 08:00:11 -0300

    fibonacci

--- clase5/clase5/src/App.java
+++ clase5/clase5/src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(fibonacci(0));
+        System.out.println(fibonacci(1));
+        System.out.println(fibonacci(2));
+        System.out.println(fibonacci(3));
+        System.out.println(fibonacci(4));
+        System.out.println(fibonacci(5));
+        System.out.println(fibonacci(6));
+        System.out.println(fibonacci(7));
+        System.out.println(fibonacci(8));
+    }
+
+    public static int fibonacci(int numero) {
+        if(numero<=0) return 0;
+        if(numero==1) return 1;
+        if(numero==2) return 1;
+        return fibonacci(numero-1)+fibonacci(numero-2);
     }
 }
